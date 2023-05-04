b93b4a2890a5cbb98f0de6db90af6751f2ee4216
sowmya <sowmyasoma2023@gmail.com>
2023-05-04 15:49:12 +0530

    signup without username

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -64,7 +64,7 @@ const Signup = () => {
     e.preventDefault();
     const payload = {
 
-      "username": username,
+      // "username": username,
 
       "email": email,
 
@@ -122,7 +122,7 @@ const Signup = () => {
 
                 <form>
                   <div>
-                    <div className="mb-3" id="formBasicUsername">
+                    {/* <div className="mb-3" id="formBasicUsername">
                       <label>Username</label>
                       <input
                         onChange={(e) => validateEmail(e)}
@@ -132,7 +132,7 @@ const Signup = () => {
                         className="form-control"
                         required
                       />
-                    </div>
+                    </div> */}
 
                     <div className="mb-3" id="formBasicEmail">
                       <label>Email address</label>
